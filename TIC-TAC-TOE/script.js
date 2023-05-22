170f10aad51f2437f099d811447b86fb4d42003a
Souvik <souvikkarmakar8642@gmail.com>
2023-05-22 21:49:16 +0530

    Added the draw condition

--- TIC-TAC-TOE/script.js
+++ TIC-TAC-TOE/script.js
@@ -3,39 +3,46 @@ window.alert("Welcome to tic tac toe")
 
 let buttonSound = new Audio("./assets/smb_bump.wav");
 let gameMusic = new Audio("./assets/8-bit-arcade-138828.mp3");
-let drawSound = new Audio("./assets/smb_fireball.wav")
+let turnSound = new Audio("./assets/smb_fireball.wav")
 // music.play();
 
 let turn = "X";
 let gameover = false;
-const changeTurn = () =>{
+const changeTurn = () => {
     return turn === "X" ? "O" : "X";
 }
+var stepsPlayed = 0;
 
-const checkWin = () =>{
+
+const checkWin = () => {
 
     let boxesText = document.getElementsByClassName("boxtext");
 
     let wins = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ]
 
-    wins.forEach(e =>{
-        if((boxesText[e[0]].innerText === boxesText[e[1]].innerText) && (boxesText[e[2]].innerText === boxesText[e[1]].innerText) && (boxesText[e[0]].innerText !== "")){
+    wins.forEach(e => {
+        if ((boxesText[e[0]].innerText === boxesText[e[1]].innerText) && (boxesText[e[2]].innerText === boxesText[e[1]].innerText) && (boxesText[e[0]].innerText !== "")) {
             document.getElementsByClassName("info")[0].innerText = boxesText[e[0]].innerText + " is the winner";
             gameover = true;
-            document.querySelector('.imageBox').getElementsByTagName('img')[0].style.width = "20vw";
+            // document.querySelector('.imageBox').getElementsByTagName('img')[0].style.width = "20vw";
+            $("#win").css("width", "20vw");
         }
     })
 }
 
+checkDraw = () => {
+    $(".info").text("Draw");
+    $("#draw").css("width", "20vw");
+}
 
 
 
@@ -43,36 +50,44 @@ let boxes = document.getElementsByClassName("box");
 
 Array.from(boxes).forEach(element => {
     let boxText = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        drawSound.play();
-        if(boxText.innerText === ''){
-            
+    element.addEventListener('click', () => {
+        turnSound.play();
+        if (boxText.innerText === '') {
+            stepsPlayed++;
             boxText.innerText = turn;
             turn = changeTurn();
-            
+
             checkWin();
-            if(!gameover){
+
+            // Draw game
+            if (stepsPlayed === 9 && !gameover) {
+                checkDraw();
+                return;
+            }
+            else if (!gameover && stepsPlayed < 9) {
                 document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
             }
-            else{
+            else {
                 return;
             }
         }
     })
 })
 
-
-reset.addEventListener('click', ()=>{
-    document.querySelector('.imageBox').getElementsByTagName('img')[0].style.width = "0";
+// Reset function
+reset.addEventListener('click', () => {
+    // document.querySelector('.imageBox').getElementsByTagName('img')[0].style.width = "0";
+    $(".resultImg").css("width", "0");
     buttonSound.play()
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(e => {
         e.innerText = "";
     })
-    
-    turn = "X"
+
+    turn = "X";
+    stepsPlayed = 0;
     gameover = false;
-    if(!gameover){
+    if (!gameover) {
         document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
     }
 })
@@ -80,14 +95,16 @@ reset.addEventListener('click', ()=>{
 
 // gameMusic.play();
 let counter = 1;
-music.addEventListener('click', ()=> {
+music.addEventListener('click', () => {
     counter++;
-    if(counter %2 != 0){
+    if (counter % 2 != 0) {
         gameMusic.pause();
         document.querySelector('#music').innerText = "🔇";
     }
-    else{
+    else {
         gameMusic.play();
         document.querySelector('#music').innerText = "🔊";
     }
 })
+
+
